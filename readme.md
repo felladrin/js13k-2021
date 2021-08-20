36e21e6bb54854036aabd7caaf2a9b7478d58908
Victor Nogueira <felladrin@gmail.com>
2021-08-21 02:59:36 +0300

    Replace the static character by an animated one

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # js13k-2021
 
 Game created for the Js13kGames competition from 2021.
+
+## Credits
+
+SVG art created on [SVG-Edit](https://github.com/SVG-Edit/svgedit), optimized with [SVGOMG](https://jakearchibald.github.io/svgomg/) and converted to canvas code using [SVG to HTML5 Canvas Converter](http://www.professorcloud.com/svg-to-canvas/).
+
+2D Pixel Art Cat Sprites: https://elthen.itch.io/2d-pixel-art-cat-sprites
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,16 @@
 import { createPubSub, createPubSub as store } from "create-pubsub";
 import { initFont, font } from "tinyfont";
-import { GameLoop, GameObject, getPointer, init, Pool, Sprite } from "kontra";
+import {
+  GameLoop,
+  GameObject,
+  getPointer,
+  init,
+  Pool,
+  Sprite,
+  SpriteSheet,
+  loadImage,
+} from "kontra";
+import catSpriteSheet from "./cat-sprite-sheet.png";
 
 export const { canvas, context } = init("game");
 
@@ -19,58 +29,21 @@ export const [setGameObjectDragged, , getGameObjectDragged] = store<any>(null);
 
 export const [setTimeInGame, onTimeInGameChanged, getTimeInGame] = store(0);
 
-export const gameObject = GameObject({
+export const gameObject = Sprite({
   x: canvas.width / 2,
   y: canvas.height / 2,
-  width: 31,
-  height: 82,
   scaleX: 2,
   scaleY: 2,
-  anchor: { x: 0.5, y: 0.5 },
-  props: {
-    defaultColor: "gray",
-    color: "gray",
-  },
-  onOver: () => {
-    if (getGameObjectDragged() !== null) return;
-    gameObject.props.color = "red";
-  },
-  onOut: () => {
-    if (getGameObjectDragged() !== null) return;
-    gameObject.props.color = gameObject.props.defaultColor;
-  },
+  anchor: { x: 0.5, y: 1 },
   onUp: () => {
     if (getGameObjectDragged() === null) return;
     setGameObjectDragged(null);
-    gameObject.props.color = gameObject.props.defaultColor;
+    gameObject.playAnimation("falling");
   },
   onDown: () => {
     if (getGameObjectDragged() !== null) return;
     setGameObjectDragged(gameObject);
-    gameObject.props.color = "green";
-  },
-  render: () => {
-    const ctx = gameObject.context;
-    ctx.fillStyle = gameObject.props.color;
-    ctx.strokeStyle = "white";
-    ctx.beginPath();
-    ctx.moveTo(15, 0);
-    ctx.bezierCurveTo(19.14, 0, 22.5, 3.35, 22.5, 7.5);
-    ctx.bezierCurveTo(22.5, 11.64, 19.14, 15, 15, 15);
-    ctx.bezierCurveTo(10.85, 15, 7.5, 11.64, 7.5, 7.5);
-    ctx.bezierCurveTo(7.5, 3.35, 10.85, 0, 15, 0);
-    ctx.moveTo(15, 15);
-    ctx.lineTo(15, 40);
-    ctx.moveTo(15, 20);
-    ctx.lineTo(0, 20);
-    ctx.moveTo(15, 20);
-    ctx.lineTo(30, 20);
-    ctx.moveTo(15, 40);
-    ctx.lineTo(0, 60);
-    ctx.moveTo(15, 40);
-    ctx.lineTo(30, 60);
-    ctx.fill();
-    ctx.stroke();
+    gameObject.playAnimation("dragged");
   },
   update: () => {
     gameObject.advance();
@@ -78,14 +51,82 @@ export const gameObject = GameObject({
     if (getGameObjectDragged() === gameObject) {
       gameObject.x = getPointer().x;
       gameObject.y = getPointer().y;
-    } else if (gameObject.y + gameObject.height / 2 < canvas.height) {
+    } else if (gameObject.y < canvas.height) {
       gameObject.ddy = 0.5;
     } else {
+      gameObject.playAnimation("idleOne");
+      gameObject.y = canvas.height;
       gameObject.dy = 0;
       gameObject.ddy = 0;
     }
   },
 });
+
+(async () => {
+  let spriteSheet = SpriteSheet({
+    image: await loadImage(catSpriteSheet),
+    frameWidth: 32,
+    frameHeight: 32,
+    animations: {
+      idleOne: {
+        frames: "0..3",
+        frameRate: 8,
+      },
+      idleTwo: {
+        frames: "8..11",
+        frameRate: 8,
+      },
+      idleThree: {
+        frames: "16..19",
+        frameRate: 8,
+      },
+      idleFour: {
+        frames: "24..27",
+        frameRate: 8,
+      },
+      walk: {
+        frames: "32..39",
+        frameRate: 8,
+      },
+      jumpOne: {
+        frames: "40..43",
+        frameRate: 8,
+      },
+      jumpTwo: {
+        frames: "44..47",
+        frameRate: 8,
+      },
+      rest: {
+        frames: "48..51",
+        frameRate: 2,
+      },
+      poke: {
+        frames: "56..61",
+        frameRate: 8,
+      },
+      attack: {
+        frames: "64..70",
+        frameRate: 8,
+      },
+      scare: {
+        frames: "72..79",
+        frameRate: 8,
+      },
+      dragged: {
+        frames: "64..65",
+        frameRate: 8,
+        loop: false,
+      },
+      falling: {
+        frames: "66..70",
+        frameRate: 8,
+        loop: false,
+      },
+    },
+  });
+
+  gameObject.animations = spriteSheet.animations;
+})();
 
 export const textObject = GameObject({
   x: 10,
@@ -93,7 +134,7 @@ export const textObject = GameObject({
   props: {
     text: "TIME: 0",
     color: "white",
-    size: 20,
+    size: 10,
   },
   render: () => {
     renderText(
