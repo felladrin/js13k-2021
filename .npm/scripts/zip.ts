4e631575515ed47ec5f2e2b449f5a77266667ff4
Victor Nogueira <felladrin@gmail.com>
2021-08-22 01:22:26 +0300

    Display the size of the build based on the zip file size instead of Vite's output

--- .npm/scripts/zip.ts
+++ .npm/scripts/zip.ts
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import zip from "bestzip";
 import efficientCompressionTool from "ect-bin";
 import crossExecFile from "cross-exec-file";
+import zipstats from "zipstats";
 
 const distFolderPath = resolve(__dirname, "..", "..", "dist");
 const zippedFilePath = resolve(__dirname, "..", "..", "dist.zip");
@@ -35,7 +36,7 @@ const zippedFilePath = resolve(__dirname, "..", "..", "dist.zip");
   const status = isUnderSizeLimit ? "under" : "over";
   const statusColor = isUnderSizeLimit ? "\x1b[32m" : "\x1b[31m";
 
-  console.log(`File path: ${zippedFilePath}`);
-  console.log(`File size: ${fileSize} Bytes`);
+  console.log(`${zippedFilePath} (${fileSize} bytes)`);
+  console.log(zipstats(zippedFilePath));
   console.log(`${statusColor}%s\x1b[0m`, `Status: ${fileSizeDifference} bytes ${status} the 13KB limit!`);
 })();
