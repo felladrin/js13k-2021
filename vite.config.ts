43844984ac6f8f76412359c300b08d3d2066733c
Victor Nogueira <felladrin@gmail.com>
2021-09-04 21:25:41 +0300

    Use 'rollup-plugin-kontra' to reduce even more the bundle size
    Reference: https://straker.github.io/kontra/reducing-file-size

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import { minifyHtml } from "vite-plugin-html";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import kontra from "rollup-plugin-kontra";
 import { Packer, InputType, InputAction } from "roadroller";
 
 export default defineConfig((configEnv) => ({
   plugins: [
+    kontra({
+      debug: configEnv.command === "serve",
+      gameObject: {
+        acceleration: true,
+        anchor: true,
+        camera: false,
+        group: false,
+        opacity: false,
+        rotation: false,
+        scale: true,
+        ttl: true,
+        velocity: true,
+      },
+      sprite: {
+        animation: true,
+        image: true,
+      },
+      text: { auto: false, newline: false, rtl: false, text: false },
+      vector: {
+        angle: false,
+        clamp: false,
+        distance: false,
+        dot: false,
+        length: false,
+        normalize: false,
+        scale: false,
+        subtract: false,
+      },
+    }),
     {
       name: "roadroller",
       async transformIndexHtml(html, context) {
