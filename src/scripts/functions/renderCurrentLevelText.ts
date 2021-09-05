39cf042fea23af84860c7c1da4db2475d523cabf
Victor Nogueira <felladrin@gmail.com>
2021-09-05 23:24:10 +0300

    Move Current Level Text to the bottom bar

--- src/scripts/functions/renderCurrentLevelText.ts
+++ src/scripts/functions/renderCurrentLevelText.ts
@@ -3,5 +3,5 @@ import { canvas, renderText } from "../instances";
 import { getCurrentLevel } from "../stores";
 
 export function renderCurrentLevelText() {
-  renderText(`LEVEL ${getCurrentLevel()}`, canvas.width / 2 - 30, 10, 10, yellowGreenCrayola);
+  renderText(`LEVEL ${getCurrentLevel()}`, canvas.width / 4 - 18, canvas.height - 28, 10, yellowGreenCrayola);
 }
