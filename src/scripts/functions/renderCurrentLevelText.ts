c8487eac2c66ce12a3da06fea9182ef78d14a05a
Victor Nogueira <felladrin@gmail.com>
2021-09-07 01:06:14 +0300

    Display the label "Quadrant" instead of "Level" on bottom bar

--- src/scripts/functions/renderCurrentLevelText.ts
+++ src/scripts/functions/renderCurrentLevelText.ts
@@ -1,7 +1,9 @@
 import { yellowGreenCrayola } from "../colors";
 import { canvas, renderText } from "../instances";
 import { getCurrentLevel } from "../stores";
+import { to as convertNumberToString } from "base26";
 
 export function renderCurrentLevelText() {
-  renderText(`LEVEL ${getCurrentLevel()}`, canvas.width / 4 - 18, canvas.height - 28, 10, yellowGreenCrayola);
+  const quadrant = convertNumberToString(getCurrentLevel() + 1).toUpperCase();
+  renderText(`QUADRANT: ${quadrant}`, canvas.width / 4 - 30, canvas.height - 28, 10, yellowGreenCrayola);
 }
