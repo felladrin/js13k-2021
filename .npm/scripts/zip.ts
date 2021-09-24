b81b1256b43d280dc02cf5e26650d45328589e31
Victor Nogueira <felladrin@gmail.com>
2021-09-25 00:21:05 +0300

    Use "tasuku" to separate the steps in zip script

--- .npm/scripts/zip.ts
+++ .npm/scripts/zip.ts
@@ -1,62 +1,60 @@
-import { statSync, unlinkSync, existsSync } from "fs";
-import { resolve } from "path";
-import { createPubSub } from "create-pubsub";
-import { greenBright, redBright } from "colorette";
 import zip from "bestzip";
-import efficientCompressionTool from "ect-bin";
+import { greenBright, redBright } from "colorette";
 import crossExecFile from "cross-exec-file";
+import efficientCompressionTool from "ect-bin";
+import { existsSync, statSync, unlinkSync } from "fs";
+import { EOL } from "os";
+import { resolve } from "path";
+import task from "tasuku";
 import zipstats from "zipstats";
 
 const distFolderPath = resolve(__dirname, "..", "..", "dist");
 const zippedFilePath = resolve(__dirname, "..", "..", "dist.zip");
-const [emitZipProcessInitialized, onZipProcessInitialized] = createPubSub();
-const [emitOldZipRemoved, onOldZipRemoved] = createPubSub();
-const [emitZipCreated, onZipCreated] = createPubSub();
-const [emitZipOptimized, onZipOptimized] = createPubSub();
 
-onZipProcessInitialized(() => {
-  if (existsSync(zippedFilePath)) {
-    unlinkSync(zippedFilePath);
-  }
+task.group((task) => [
+  task("Delete old zip file", async ({ setOutput }) => {
+    if (existsSync(zippedFilePath)) {
+      unlinkSync(zippedFilePath);
+      setOutput("Old zip file deleted.");
+    } else {
+      setOutput("Zip file not created yet.");
+    }
+  }),
+  task("Create zip file", async ({ setOutput }) => {
+    await zip({
+      cwd: distFolderPath,
+      source: "index.html",
+      destination: zippedFilePath,
+    });
 
-  emitOldZipRemoved();
-});
+    const fileSize = statSync(zippedFilePath).size;
 
-onOldZipRemoved(async () => {
-  await zip({
-    cwd: distFolderPath,
-    source: "index.html",
-    destination: zippedFilePath,
-  });
+    setOutput(`${zippedFilePath} (${fileSize} bytes)`);
+  }),
+  task("Re-compress with Efficient Compression Tool", async ({ setOutput, setWarning }) => {
+    try {
+      const { stdout } = await crossExecFile(efficientCompressionTool, ["-9", "-zip", zippedFilePath]);
+      const fileSize = statSync(zippedFilePath).size;
 
-  emitZipCreated();
-});
+      setOutput([stdout, `New zip file size: ${fileSize} bytes`].join(EOL));
+    } catch {
+      setWarning("Skipping usage of Efficient Compression Tool.");
+    }
+  }),
+  task("Check zip file content", async ({ setOutput }) => {
+    setOutput(zipstats(zippedFilePath));
+  }),
+  task("Check status", async ({ setOutput, setError }) => {
+    const maxSizeAllowed = 13 * 1024;
+    const fileSize = statSync(zippedFilePath).size;
+    const fileSizeDifference = Math.abs(maxSizeAllowed - fileSize);
+    const isUnderSizeLimit = fileSize <= maxSizeAllowed;
+    const message = `${fileSizeDifference} bytes ${isUnderSizeLimit ? "under" : "over"} the 13KB limit!`;
 
-onZipCreated(async () => {
-  console.log("Zip file created successfully!");
-
-  try {
-    console.log("Running Efficient Compression Tool to reduce the zip file size.");
-    const { stdout } = await crossExecFile(efficientCompressionTool, ["-9", "-zip", zippedFilePath]);
-    console.log(stdout);
-  } catch {
-    console.log("Skipping usage of Efficient Compression Tool.");
-  }
-
-  emitZipOptimized();
-});
-
-onZipOptimized(() => {
-  const maxSizeAllowed = 13 * 1024;
-  const fileSize = statSync(zippedFilePath).size;
-  const fileSizeDifference = Math.abs(maxSizeAllowed - fileSize);
-  const isUnderSizeLimit = fileSize <= maxSizeAllowed;
-  const status = isUnderSizeLimit ? "under" : "over";
-  const colorize = isUnderSizeLimit ? greenBright : redBright;
-
-  console.log(`${zippedFilePath} (${fileSize} bytes)`);
-  console.log(zipstats(zippedFilePath));
-  console.log(colorize(`Status: ${fileSizeDifference} bytes ${status} the 13KB limit!`));
-});
-
-emitZipProcessInitialized();
+    if (isUnderSizeLimit) {
+      setOutput(greenBright(message));
+    } else {
+      setError(redBright(message));
+    }
+  }),
+]);
